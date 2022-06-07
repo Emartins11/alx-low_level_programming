96ab38deb8e8eea841b7ee651884163f1129a96f
Emartins <emartinsebuka@gmail.com>
2022-06-06 17:09:40 -0700

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+* main - Entry
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int x;
+for (x = '00'; x <= '99'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+
+return (0);
+}
