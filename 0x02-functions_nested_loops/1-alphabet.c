1d9670fcf5709dd2a2a8564dcda6f41f3c52aa97
Ebuka Martins <emartinsebuka@gmail.com>
2022-06-08 06:53:12 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,13 +9,15 @@
 
 void print_alphabet(void)
 {
-int x;
-for (x = 'a'; x <= 'z'; x++)
-{
-putchar(x);
-}
-putchar('\n');
+char c = 'a';
 
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+
+_putchar('\n');
 }
 
 int main(void)
