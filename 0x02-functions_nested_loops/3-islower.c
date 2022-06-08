274e5ba40e3d7242664ad2d89616145ece035078
Emartins <emartinsebuka@gmail.com>
2022-06-08 10:34:50 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
-* _islower - prints 1 if lower and 0 if not
+* _islower - returns 1 if lower and 0 if not
 */
 
 int _islower(int c)
 {
 if  (c >= 'a' && c <= 'z')
-return 1;
+return (1);
 else
-return 0;
+return (0);
 
 }
