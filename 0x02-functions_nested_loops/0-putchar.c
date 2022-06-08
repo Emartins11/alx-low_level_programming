cfc8adde8a66ed238f6ebfc36de583fbb50d3137
Emartins <emartinsebuka@gmail.com>
2022-06-07 20:02:09 -0700

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<main.h>
+
+/**
+* main - Entry
+*
+* Return: Always 0
+*/
+
+void print_putchar(void);
+{
+char a[10];
+a = "_putchar\n";
+return a;
+}
+
+int main(void)
+{
+print_putchar();
+
+return (0);
+}
