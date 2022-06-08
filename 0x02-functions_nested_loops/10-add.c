5840263b3a05e57e055e7478330c41d2ff055f77
Emartins <emartinsebuka@gmail.com>
2022-06-08 14:04:56 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
 * add - check if a character is an alphabet
 * @a: the character to be checked
 * @b: the character to be checked
-
+*
 * Return: 1 if lower and 0 if not
 */
 
@@ -13,6 +13,6 @@ int add(int a, int b)
 {
 int result;
 result = a + b;
-return result;
+return (result);
 
 }
