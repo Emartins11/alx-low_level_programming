7a07cd3ca9ad0b2e6b061fd281575096b27d2d70
Emartins <emartinsebuka@gmail.com>
2022-06-08 23:57:13 -0700

    changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,48 @@
 #include "main.h"
 
 /**
-* _abs - check if a character is an alphabet
+* print_times_table - check if a character is an alphabet
 * @n: the character to be checked
 * Return: 1 if lower and 0 if not
 */
 
-int _abs(int n)
+void print_times_table(int n)
 {
-if  (n >= 0)
-return (n);
+if (n <= 15 && n >= 0)
+int i;
+int j;
+for (i = 0; i <= n; i++)
+{
+if (i == 0)
+{
+for (j = 0; j <= i + n; j++)
+{
+printf("%d", i);
+if (j < i + n)
+{
+putchar(',');
+putchar(' ');
+putchar(' ');
+}
+}
+}
 else
-return (-1 * n);
+{
+for (j = 0; j <= i * n; j++)
+{
+if (j % i == 0)
+{
+printf("%d", j);
+if (j < i * n)
+{
+putchar(',');
+putchar(' ');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+}
 
 }
