e031c928827b6e33acf0517b89d5b7a09b2e99b1
Emartins <emartinsebuka@gmail.com>
2022-06-08 22:45:55 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,23 +2,28 @@
 #include "main.h"
 
 /**
-* main - Entry
+* _abs - check if a character is an alphabet
 *
-* Return: Always 0
+* Return: 1 if lower and 0 if not
 */
 
 int main(void)
 {
+int x;
+int f1;
+int f2;
+int nxt;
+f1 = 1;
+f2 = 2;
 
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+printf("%d", f1)
+for (x = 2; x <= 50; x++)
+{
+printf("%d", f2)
+nxt = f1 +f2;
+f1 = f2;
+f2 = nxt;
+}
 
 return (0);
 }
