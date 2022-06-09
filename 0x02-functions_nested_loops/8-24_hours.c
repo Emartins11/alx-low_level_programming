ff011112fc3bb196b9896dd5abafea2c479308d5
Emartins <emartinsebuka@gmail.com>
2022-06-08 20:18:14 -0700

    changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,23 +2,33 @@
 #include "main.h"
 
 /**
-* main - Entry
-*
-* Return: Always 0
+* jack_bauer - check if a character is an alphabet
+* 
+* Return: 1 if lower and 0 if not
 */
 
-int main(void)
+void jack_bauer(void);
 {
+int x;
+int y;
+int a;
+int b;
+for (x = 0; x <= 2; x++)
+{
+for (y = 0; y <= 3; y++)
+{
+for (a = 0; a <= 5; a++)
+{
+for (b - 0; a <=9; b++)
+{
+_putchar(x);
+_putchar(y);
+_putchar(':');
+_putchar(a);
+_putchar(b);
+}
+}
+}
+}
 
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-
-return (0);
 }
