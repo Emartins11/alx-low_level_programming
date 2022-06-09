a55fb6f10124b9aeece87645abae530eb917e00c
Emartins <emartinsebuka@gmail.com>
2022-06-08 20:53:14 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,22 @@
 void print_to_98(int n)
 {
 int x;
-if (n <= '98')
+if (n <= 98)
 {
-for (x = n; x <= '98'; x++)
+for (x = n; x <= 98; x++)
 {
-_putchar(x);
-_putchar(',');
-_putchar(' ');
+putchar(x);
+putchar(',');
+putchar(' ');
 }
 }
 else 
 {
-for (x = n; x >= '98'; x--)
+for (x = n; x >= 98; x--)
 {
-_putchar(x);
-_putchar(',');
-_putchar(' ');
+putchar(x);
+putchar(',');
+putchar(' ');
 }
 }
 
