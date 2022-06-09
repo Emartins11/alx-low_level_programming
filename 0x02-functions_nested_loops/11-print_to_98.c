1415a26790d8516aa81a8574e848b9fe5f308317
Emartins <emartinsebuka@gmail.com>
2022-06-08 20:45:19 -0700

    changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,23 +2,31 @@
 #include "main.h"
 
 /**
-* main - Entry
-*
-* Return: Always 0
+* print_to_98 - check if a character is an alphabet
+* Return: 1 if lower and 0 if not
 */
 
-int main(void)
+void print_to_98(int n)
 {
-
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-
-return (0);
+int x;
+if (n <= 98)
+{
+for (x =n; x <= 98; x++)
+{
+_putchar(x);
+_putchar(',');
+_putchar(' ');
 }
+}
+else 
+{
+for (x = n; x >= 98; x--)
+{
+_putchar(x);
+_putchar(',');
+_putchar(' ');
+}
+}
+
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,23 +2,36 @@
 #include "main.h"
 
 /**
-* main - Entry
-*
-* Return: Always 0
+* times_table - check if a character is an alphabet
+* Return: 1 if lower and 0 if not
 */
 
-int main(void)
+void times_table(void);
 {
+int i;
+int j;
+for (i = 0; i <= 9; i++)
+{
+if (i == 0)
+{
+for (j = 0; j <= i + 9; j++)
+{
+_putchar(i);
+_putchar('\t');
+}
+}
+else
+{
+for (j = 0; j <= i * 9; j++)
+{
+if (j % i == 0)
+{
+_putchar(j);
+_putchar('\t');
+}
+}
+}
+_putchar('\n');
+}
 
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-
-return (0);
 }
