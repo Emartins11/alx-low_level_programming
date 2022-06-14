0409a4d887322fa43cebb471afc0aed4203c7255
Ebuka Martins <emartinsebuka@gmail.com>
2022-06-14 04:35:49 +0100

    Update 0-rest_to_98.c

--- 0x05-pointers_arrays_strings/0-rest_to_98.c
+++ 0x05-pointers_arrays_strings/0-rest_to_98.c
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "main.h"
 
+/**
+* reset_to_98 - check if a character is an alphabet
+* @*n: the character to be checked
+*
+*/
+
 void reset_to_98(int *n)
 {
 *n = 98;
