f7338513298bc905197b17d9e2c48d3d84e46450
Ebuka Martins <emartinsebuka@gmail.com>
2022-06-14 05:37:17 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*swap_int - check if a character is an alphabet
+*@a: the character to be checked
+*@b: the chance
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = swap;
+*a = *b;
+*b = swap;
+}
