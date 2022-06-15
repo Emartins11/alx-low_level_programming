0aa2f04f7811ccddb4f2ccf7d9a392889c9880da
Emartins <emartinsebuka@gmail.com>
2022-06-14 20:34:30 -0700

    ch

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+*rev_string - check if a character is an alphabet
+*@s: the character to be checked
+*
+*/
+
+void rev_string(char *s)
+{
+strrev(s);
+
+
+}
