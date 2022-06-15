c6d98eb3e557e703822f62b01ab475412e3b62c6
Emartins <emartinsebuka@gmail.com>
2022-06-14 20:54:56 -0700

    ch

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+*_atoi - check if a character is an alphabet
+*@s: the character to be checked
+*Return: anything
+*/
+
+int _atoi(char *s)
+{
+int len;
+len = strlen(s);
+return (len);
+
+
+}
