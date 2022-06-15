e993993345eb8530c447e5d6873189143ed74cc1
Emartins <emartinsebuka@gmail.com>
2022-06-14 21:01:44 -0700

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts2 - check if a character is an alphabet
+*@str: the character to be checked
+*
+*/
+
+void puts2(char *str)
+{
+printf("%s\n", str);
+
+
+}
