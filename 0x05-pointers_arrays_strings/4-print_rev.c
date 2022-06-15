0780b7cbb282983ca58d6b8d770405b84f46ee9f
Emartins <emartinsebuka@gmail.com>
2022-06-14 20:06:29 -0700

    changes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_rev - check if a character is an alphabet
+*@s: the character to be checked
+*
+*/
+
+void print_rev(char *s)
+{
+printf("%s\n", strrev(s));
+
+
+}
