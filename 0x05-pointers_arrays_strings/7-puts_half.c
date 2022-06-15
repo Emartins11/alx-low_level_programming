49fba57a87aa72327e786562058226b107599653
Emartins <emartinsebuka@gmail.com>
2022-06-14 21:02:49 -0700

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts_half - check if a character is an alphabet
+*@str: the character to be checked
+*
+*/
+
+void puts_half(char *str)
+{
+printf("%s\n", str);
+
+
+}
