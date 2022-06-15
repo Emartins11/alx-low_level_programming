9f7f0cc18fa0d4c4a841efa4cbc40f8f7ab7e5d0
Emartins <emartinsebuka@gmail.com>
2022-06-14 19:52:28 -0700

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_strlen - check if a character is an alphabet
+*@s: the character to be checked
+*return: anything
+*/
+
+int _strlen(char *s)
+{
+int len;
+len = strlen(s);
+return (len);
+
+
+}
