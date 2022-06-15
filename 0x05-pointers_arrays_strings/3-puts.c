c6373b142c01c7c0ac881d69a521456dcb358cad
Emartins <emartinsebuka@gmail.com>
2022-06-14 19:42:08 -0700

    new files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_puts - check if a character is an alphabet
+*@str: the character to be checked
+*
+*/
+
+void _puts(char *str)
+{
+printf("%s\n", str);
+
+
+}
