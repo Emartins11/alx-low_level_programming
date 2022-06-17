5f64522fe86ed9b3ca51d3f8ef1964fb3c014936
Emartins <emartinsebuka@gmail.com>
2022-06-16 21:22:23 -0700

    new files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,13 @@
 #include <string.h>
 
 /**
-*_strcpy - check if a character is an alphabet
+*_strcat - check if a character is an alphabet
 *@dest: the character to be checked
 *@src: the character to be checked
 *Return: anything
 */
 
-char *_strcpy(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 int len;
 len = strlen(src);
