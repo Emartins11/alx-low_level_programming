a7d6e9cdbda7f0913cf0c75ae3483243203e9f22
Emartins <emartinsebuka@gmail.com>
2022-06-16 21:56:12 -0700

    Update 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*reverse_array - check if a character is an alphabet
+*@a: the character to be checked
+*@n: the chance
+*/
+
+void reverse_array(int *a, int n)
+{
+*a = 38;
+n = 50;
+}
