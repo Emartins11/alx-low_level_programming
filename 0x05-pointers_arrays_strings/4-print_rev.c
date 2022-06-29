349624e836207367b345305252e1de12dc86980d
Ebuka Martins <emartinsebuka@gmail.com>
2022-06-29 05:38:35 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,35 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 
 /**
-*print_rev - check if a character is an alphabet
-*@s: the character to be checked
-*
-*/
-
+ * print_rev - prints a string in reverse.
+ *
+ * @s: store the string.
+ */
 void print_rev(char *s)
 {
+	int i;
 
-printf("%s\n", s);
+	i = string_length(s) - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
 
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
