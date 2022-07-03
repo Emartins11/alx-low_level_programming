e82e305dec8a5cce1ce6acbda69554923304d4d8
Ebuka Martins <emartinsebuka@gmail.com>
2022-07-03 03:39:06 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
 
 /**
-*rev_string - check if a character is an alphabet
-*@s: the character to be checked
-*
-*/
-
+ * rev_string - Reverses a string.
+ *
+ * string_length: prints number of characters.
+ * @s: store character.
+ * @: store character.
+ * Return: Always 0.
+ */
 void rev_string(char *s)
 {
-printf("%s\n", s);
+	int length, c;
+	char *begin, *end, temp;
 
+	length = string_length(s);
 
+	begin = s;
+	end = s;
+
+	for (c = 0; c < (length - 1); c++)
+		end++;
+
+	for (c = 0; c < length / 2; c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
