dad8e411d8f07e412dcf290a4194d6c2b7cb7252
Ebuka Martins <emartinsebuka@gmail.com>
2022-07-03 04:00:39 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,14 +2,45 @@
 #include "main.h"
 
 /**
-*puts_half - check if a character is an alphabet
-*@str: the character to be checked
-*
-*/
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
 
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * puts_half - print half of a string.
+ * @str: character
+ */
 void puts_half(char *str)
 {
-printf("%s\n", str);
+	int m, n, i, j;
 
+	m = string_length(str) / 2;
 
+	if (string_length(str) % 2 == 0)
+	{
+		for (i = m; i < string_length(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (string_length(str) % 2 != 0)
+	{
+		n = (string_length(str) - 1) / 2;
+		for (j = n; j <= string_length(str); j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
