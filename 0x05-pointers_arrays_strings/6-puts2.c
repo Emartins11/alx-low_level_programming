cd14fff0299c815e4d4cc931e2334d47a89574b1
Ebuka Martins <emartinsebuka@gmail.com>
2022-07-03 03:54:24 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,32 @@
 #include "main.h"
 
 /**
-*puts2 - check if a character is an alphabet
-*@str: the character to be checked
-*
-*/
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
 
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * puts2 - print every other character of a string.
+ * @str: character
+ */
 void puts2(char *str)
 {
-printf("%s\n", str);
+	int i;
 
-
+	for (i = 0; i < string_length(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
